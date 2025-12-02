54fe23155d0989454b65cf5c1a1458e55bc693d9
thevenardjulien <thevenard.julien@gmail.com>
2025-12-02 11:39:40 +0100

    test https://examlokijulien-frontend-dqjv0e.dokploy.app/

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'hhttps://examlokijulien-frontend-dqjv0e.dokploy.app/api';
 
 export const fetchProducts = () => axios.get(`${API_BASE_URL}/products`);
 
