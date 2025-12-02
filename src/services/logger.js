649d809feeedd07ae1ee799a0af1a5dcd67841c9
thevenardjulien <thevenard.julien@gmail.com>
2025-12-02 14:51:44 +0100

    update logger instead of winston

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,9 +1,19 @@
-import winston from 'winston';
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
-});
+// Logger simple pour le navigateur (remplace Winston qui n'est pas compatible avec le frontend)
+const logger = {
+  info: (...args) => {
+    console.log('[INFO]', ...args);
+  },
+  error: (...args) => {
+    console.error('[ERROR]', ...args);
+  },
+  warn: (...args) => {
+    console.warn('[WARN]', ...args);
+  },
+  debug: (...args) => {
+    if (process.env.NODE_ENV === 'development') {
+      console.debug('[DEBUG]', ...args);
+    }
+  },
+};
 
 export default logger;
